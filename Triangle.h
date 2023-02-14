c08b2af460a80600a084e722289a3568a9631c23
Ki4EH <67659893+Ki4EH@users.noreply.github.com>
2023-02-15 01:07:34 +0300

    S
    Test

---
+++ Triangle.h
@@ -0,0 +1,10 @@
+class Triangle {
+    private:
+        double a, b, c;
+    public:
+        bool exst_tr ();
+        void set (double, double, double);
+        void show ();
+        double perimetr();
+        double square();
+};
